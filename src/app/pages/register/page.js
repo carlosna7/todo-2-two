f69a07e35bf20187676d9070790d571d8a4d8edf
Carlos Eduardo <carloseduardo.na7@gmail.com>
2024-01-19 11:06:05 -0300

    att

--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import  { AuthContext } from '../../context/authContext'
+import { useForm } from '@/app/utility/hooks'
+import { useMutation, gql } from '@apollo/client'
 
 const register = () => {
   return (
