daceadb63a9cb1fd80a4fe5a490ae8522a594572
Carlos Eduardo <carloseduardo.na7@gmail.com>
2024-01-24 18:01:25 -0300

    att

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,25 +1,47 @@
 'use client'
 
 import { AuthContext } from '@/app/context/authContext'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const GET_USER = gql`
+    query Query($amount: Int) {
+        getUsers(amount: $amount) {
+            name
+        }
+    }
+`;
 
 const dashboard = () => {
 
     const { user, logout } = useContext(AuthContext)
 
+    const { loading, error, data } = useQuery(GET_USER, {
+        variables: { amount: 3 },
+    });
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    // const users = data.getUsers
+    console.log(data)
+    // users.forEach(user => {
+    //     console.log(user.name);
+    // });
+
     return (
+
         <>
-        
-        {user ?
-        <>
-            <p> {user.name} is logged in</p>
-        </>
-        
-        :
-        <>
-            <p>There is no user logged</p>
-        </>
-        }
+            {user ?
+            <>
+                <p> {user.name} is logged in</p>
+            </>
+            
+            :
+            <>
+                <p>There is no user logged</p>
+            </>
+            }
 
         </>
     )
