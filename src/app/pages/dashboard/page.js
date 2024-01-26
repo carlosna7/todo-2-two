dd394195aba2b30b7a413877b2dc8827b49c28e5
Carlos Eduardo <carloseduardo.na7@gmail.com>
2024-01-26 17:00:59 -0300

    att

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -3,7 +3,8 @@
 import SelectComponent from '@/app/components/SelectComponent'
 import { AuthContext } from '@/app/context/authContext'
 import React, { useContext, useState } from 'react'
-import { gql } from '@apollo/client'
+import { gql, useMutation } from '@apollo/client'
+import { useForm } from '@/app/utility/hooks'
 
 const dashboard = () => {
 
@@ -15,7 +16,8 @@ const dashboard = () => {
         setSelectedValue(selectedList)
     }
 
-    console.log(selectedValue)
+    // console.log(selectedValue)
+    const responsible = selectedValue.map(user => user.name)
 
     const CREATE_TASK = gql`
     mutation CreateTask($taskInput: TaskInput) {
@@ -28,26 +30,62 @@ const dashboard = () => {
     }     
     `
 
+    function createTaskCallback() {
+        // event.preventDefault()
+        console.log('callback aqui')
+        createTask()
+    }
+
+    const { onChange, onSubmit, values } = useForm(createTaskCallback, {
+        name: '',
+        responsible: ''
+    })
+
+    const [ createTask ] = useMutation(CREATE_TASK, {
+        update(proxy, { data: { createTask: data}}) {
+            
+        },
+        onError({ graphQlErrors }) {
+            setErrors(graphQlErrors)
+        },
+        variables: { taskInput: {
+            name: values.name,
+            responsible: responsible
+        }}
+    })
+
+    const handleSubmit = (event) => {
+        // event.preventDefault()
+
+        // const taskInput = {
+        //     name: values.name,
+        //     responsible: responsible
+        // }
+
+        // createTask({ variables: { taskInput } });
+    }
+
     return (
 
         <>
-            {user ?
-            <>
-                <p> {user.name} is logged in</p>
-            </>
-            
-            :
-            <>
-                <p>There is no user logged</p>
-            </>
-            }
+            <div className='bg-gray-100 w-96 p-4 m-4'>
+                {user ?
+                    <>
+                        <p> {user.name} is logged in</p>
+                    </>
+                
+                    :
+                    <>
+                        <p>There is no user logged</p>
+                    </>
+                }
+            </div>
 
-            
-
-            <div className='bg-gray-100 w-96 p-4 m-4 flex flex-col gap-4'>
-                <SelectComponent onSelectChange={handleSelectChange} selectedValues={selectedValue} />
+            <div className='bg-gray-100 w-96 p-4 m-4 flex flex-col gap-4' onSubmit={handleSubmit}>
+                <SelectComponent onSelectChange={handleSelectChange} selectedValues={selectedValue} onChange={onChange} />
                 <label>name</label>
-                <input></input>
+                <input type='name' name='name' onChange={onChange}/>
+                <button type="submit" onClick={onSubmit}>Submit</button>
             </div>
 
         </>
